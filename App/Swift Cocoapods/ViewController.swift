b8dfd43c5c451641bb6a54b9184f3f436df07561
cjrieck <cjrieck123@gmail.com>
2014-10-31 10:42:50 -0400

    make simple GET request with AFNetworking

--- App/Swift Cocoapods/ViewController.swift
+++ App/Swift Cocoapods/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let networkingManager = AFHTTPRequestOperationManager()
+    
+    override init() {
+        super.init(nibName: nil, bundle: nil)
+        // Do any custom initialization here
+        
+        networkingManager.GET(
+            "http://jsonip.com/",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,
+                responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+            },
+            failure: { (operation: AFHTTPRequestOperation!,
+                error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
