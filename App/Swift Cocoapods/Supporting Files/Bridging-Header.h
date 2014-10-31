254bffd87ee952517d3f882494ea3346dafc2a20
cjrieck <cjrieck123@gmail.com>
2014-10-31 10:30:38 -0400

    add bridging header to integrate Objective-C libraries in Swift project

---
+++ App/Swift Cocoapods/Supporting Files/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  Swift Cocoapods
+//
+//  Created by Clayton Rieck on 10/31/14.
+//  Copyright (c) 2014 Clayton Rieck. All rights reserved.
+//
+
+#ifndef Swift_Cocoapods_Bridging_Header_h
+#define Swift_Cocoapods_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
