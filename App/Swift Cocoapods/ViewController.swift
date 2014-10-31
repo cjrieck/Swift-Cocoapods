47acfc53763bc810ec0f5202639547b78c5d4690
cjrieck <cjrieck123@gmail.com>
2014-10-31 10:17:30 -0400

    create initial project

---
+++ App/Swift Cocoapods/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift Cocoapods
+//
+//  Created by Clayton Rieck on 10/31/14.
+//  Copyright (c) 2014 Clayton Rieck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
